2c44048ea82591270466a66a0e403b0e1609caff
schwarzening <sysuhuangn2@gmail.com>
2016-05-27 10:01:18 +0800

    Update 242.Valid Anagram.cpp

--- 242.Valid Anagram.cpp
+++ 242.Valid Anagram.cpp
@@ -1,3 +1,10 @@
+/*
+ * Given two strings s and t, write a function to determine if t is an anagram of s.
+ *
+ * For example,
+ * s = "anagram", t = "nagaram", return true.
+ * s = "rat", t = "car", return false.
+ */
 class Solution {
 public:
     bool isAnagram(string s, string t) {
