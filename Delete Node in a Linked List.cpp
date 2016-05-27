76b8318faff71e706b5597330afaf5fb0dafcca9
schwarzening <sysuhuangn2@gmail.com>
2016-05-27 19:50:49 +0800

    Update Delete Node in a Linked List.cpp

--- Delete Node in a Linked List.cpp
+++ Delete Node in a Linked List.cpp
@@ -4,7 +4,6 @@
  * Supposed the linked list is 1 -> 2 -> 3 -> 4 and you are given the third node with value 3, the linked list 
  * should become 1 -> 2 -> 4 after calling your function.
  */
-
 /**
  * Definition for singly-linked list.
  * struct ListNode {
